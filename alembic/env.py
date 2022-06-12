82ac40d374e3ab045f02806714fa4d60fb0bb63b
adomhamza <liveghost93@gmail.com>
2022-06-12 07:22:05 +0000

    modified procfile

--- alembic/env.py
+++ alembic/env.py
@@ -10,8 +10,11 @@ from app.config import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-URI = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
-config.set_main_option("sqlalchemy.url", URI)
+
+config.set_main_option(
+    "sqlalchemy.url",
+    f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}",
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
