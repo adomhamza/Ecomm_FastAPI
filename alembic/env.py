631f6dfdd166cddc77a803e6963467425c154aa3
adomhamza <liveghost93@gmail.com>
2022-06-12 08:02:28 +0000

    Modified config for heroku

--- alembic/env.py
+++ alembic/env.py
@@ -13,7 +13,7 @@ config = context.config
 
 config.set_main_option(
     "sqlalchemy.url",
-    f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}",
+    f"postgres://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}",
 )
 
 # Interpret the config file for Python logging.
